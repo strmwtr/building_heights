9e151729fb8bee07441547e65e69180cfb1177e1
Bob Brown <rb3@tutanota.com>
2017-09-07 10:00:45 -0400

    Rewrote most functions to improve modulatrity

--- building_heights.py
+++ building_heights.py
@@ -1,52 +1,63 @@
 import arcpy
 
-# Setup Workspace
-gdb = r'C:\Users\brownr\Desktop\building_heights\building_heights_data\building_heights.gdb'
-arcpy.env.workspace = gdb
-arcpy.env.overwriteOutput = True
-
 # Aquire Licenses
 arcpy.CheckOutExtension('Spatial')
 arcpy.CheckOutExtension('3D')
 
-last_return_raster = r'C:\Users\brownr\Desktop\building_heights\building_heights_data\lastreturn'
-ground_return_raster = r'C:\Users\brownr\Desktop\building_heights\building_heights_data\groundreturn'
-building_footprints = r'C:\Users\brownr\Desktop\building_heights\building_heights_data\building_heights.gdb\buildings'
-output_dir = r'C:\Users\brownr\Desktop\building_heights\building_heights_data'
+last_return_raster = r'C:\Users\brownr\Desktop\building_heights_data\source_raster\last_return'
+ground_return_raster = r'C:\Users\brownr\Desktop\building_heights_data\source_raster\ground_return'
+building_footprints = r'C:\Users\brownr\Desktop\building_heights_data\building_heights.gdb\buildings'
+output_dir = r'C:\Users\brownr\Desktop\building_heights_data\outputs3'
 prj = r'\\metanoia\geodata\PW\sw_tech\Coordinate_Systems\NAD 1983 StatePlane Virginia South FIPS 4502 (US Feet).prj'    
-def proj_rast():
-    ground_return_raster_filled = arcpy.sa.Fill(ground_return_raster)
-    last_return_raster_filled = arcpy.sa.Fill(last_return_raster)
-    ground_return_raster_projected = output_dir + '\\gr_proj'
-    last_return_raster_projected = output_dir + '\\lr_proj'
-    arcpy.ProjectRaster_management(ground_return_raster_filled, ground_return_raster_projected, prj)
-    arcpy.ProjectRaster_management(last_return_raster_filled, last_return_raster_projected, prj)
 
-def struc():
-    # Create copy of footprint layer. Bypasses editing rights for adding and 
-    # Calculating num_points field
-    footprint_source = building_footprints
-    footprint = output_dir + '\\building_footprints.shp'
-    arcpy.CopyFeatures_management(footprint_source, footprint)
-    # Adds Num_Points field to copy
-    arcpy.AddField_management(footprint, 'Num_Points', 'LONG')
-    # Calculates Num_Points to be 10% of structure area
-    arcpy.CalculateField_management(footprint, 'Num_Points',
+def delta_raster(in_raster_1, in_raster_2):
+    ''' Equivalent to Raster Calculator expression "in_raster_1 - in_raster_2
+
+        Subtracts in_raster_1 from in_raster_2 and stores output at 
+        output_dir\delta_raster
+    '''
+    delta_raster = arcpy.sa.Minus(in_raster_1, in_raster_2)
+    delta_raster.save(output_dir + '\\delta_raster')
+    return delta_raster
+
+def fill(in_raster):
+    ''' Fills raster '''
+    filled_raster = arcpy.sa.Fill(in_raster)
+    return filled_raster
+
+def proj_rast(in_raster, out_raster, prj):
+    ''' Projects raster '''
+    arcpy.ProjectRaster_management(in_raster, out_raster, prj)
+
+def random_points(feature, out_location):
+    temp = out_location + '\\temp.shp'
+    arcpy.CopyFeatures_management(feature, temp)
+    arcpy.AddField_management(temp, 'Num_Points', 'LONG')
+    arcpy.CalculateField_management(temp, 'Num_Points', 
     '[Shape_Area]*0.1', 'VB')
-    # Creates Ran_Points layer based on Num_Points field in Structures
-    arcpy.CreateRandomPoints_management(output_dir,'Ran_Points.shp', footprint,
+    arcpy.CreateRandomPoints_management(output_dir,'Ran_Points.shp', temp,
     number_of_points_or_field='Num_Points')
-    # Creates features to hold BIN info from Structures and location Ran_Points
-    int_list = [footprint, output_dir+'\\Ran_Points.shp']
-    arcpy.Intersect_analysis(int_list, output_dir+'\\Points_G_Int.shp')
-    arcpy.Intersect_analysis(int_list, output_dir+'\\Points_LR_Int')
-    # Adds elevation data to each point, one set for ground points, one set for
-    # last return points. Last return points are used to cut out the noise of
-    # trees, birds, and other anomolies in LIDAR data
-    arcpy.AddSurfaceInformation_3d(output_dir+'\\Points_G_Int.shp', output_dir + '\\gr_proj', 'Z')
-    arcpy.AddSurfaceInformation_3d(output_dir+'\\Points_LR_Int.shp', output_dir + '\\lr_proj', 'Z')
 
-struc()
+def elevation_data(in_feature, in_raster):
+    arcpy.AddSurfaceInformation_3d(in_feature, in_raster, 'Z')
+
+def footprint_elevation(footprint, elevation_points, out_location, unique_id):
+    arcpy.Intersect_analysis([footprint, elevation_points], out_location+'\\footprint_points.shp')
+    arcpy.Statistics_analysis(out_location+'\\footprint_points.shp', out_location+'\\Ground_Stats', [['Z','MEAN']],
+    unique_id)
+
+def create_building_heights_feature(in_feature_1, in_feature_2, unique_id, out_location):
+    arcpy.JoinField_management(in_feature_1, unique_id, in_feature_2, unique_id)
+
+    arcpy.FeatureClassToFeatureClass_conversion (in_feature_1, out_location,
+    'Building_Heights.shp')
+
+
+delta_raster(last_return_raster,ground_return_raster)
+random_points(building_footprints, output_dir)
+elevation_data(output_dir+'\\Ran_Points.shp', output_dir+'\\delta_raster')
+footprint_elevation(building_footprints, output_dir+'\\Ran_Points.shp', output_dir, 'BIN')
+create_building_heights_feature(output_dir+'\\temp.shp', output_dir+'\\ground_stats', 'BIN', output_dir)
 
 # Returns licenses
 arcpy.CheckInExtension('Spatial')
