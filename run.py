a9e2f54efa6653c2f33ba5432f9f3c150cd27459
Bob Brown <rb3@tutanota.com>
2017-10-09 11:59:38 -0400

    Added add gdb feature and environments

--- run.py
+++ run.py
@@ -10,7 +10,21 @@ delta = r'\\metanoia\geodata\PW\sw_tech\Charlottesville_Lidar\DEM\delta_sp_f'
 prj = r'\\metanoia\geodata\PW\sw_tech\Coordinate_Systems\NAD 1983 StatePlane Virginia South FIPS 4502 (US Feet).prj'    
 struc = r'Database Connections\Connection to GISPRDDB direct connect.sde\cvgis.CITY.Buildings\cvgis.CITY.structure_existing_area'
 
-#bh.random_points(struc, dir_path)
-#bh.elevation_data(dir_path + r'\Ran_Points.shp', delta)
-bh.footprint_elevation(dir_path + r'\temp.shp', dir_path + r'Ran_Points', 
-  dir_path, 'BIN')
+#Create gdb to hold data
+gdb = dir_path + r'\Building_Heights.gdb'
+arcpy.CreateFileGDB_management(dir_path, 'Building_Heights.gdb')
+#Set gdb as workspace
+arcpy.env.workspace = gdb
+
+#Create random points layer
+bh.random_points(struc, 'random_points')
+#Add elevation data to random points layer
+bh.elevation_data('random_points', delta)
+#Interset random_points layer with footprints 
+bh.footprint_elevation('structure', 'random_points', 
+  gdb, 'BIN')
+
+
+# Returns licenses
+arcpy.CheckInExtension('Spatial')
+arcpy.CheckInExtension('3D')    
