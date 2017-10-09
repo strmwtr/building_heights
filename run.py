e1db229ef22c5a3e5d46caa46e51b43c48ffa64d
Bob Brown <rb3@tutanota.com>
2017-10-09 07:28:59 -0400

    Added imports and data pointers to imp.py

---
+++ run.py
@@ -0,0 +1,6 @@
+import arcpy
+import building_heights as bh
+
+dir_path = r'C:\Users\brownr\Desktop\bh\bh_data'
+delta = r'\\metanoia\geodata\PW\sw_tech\Charlottesville_Lidar\DEM\delta_sp_f'
+prj = r'\\metanoia\geodata\PW\sw_tech\Coordinate_Systems\NAD 1983 StatePlane Virginia South FIPS 4502 (US Feet).prj'    
